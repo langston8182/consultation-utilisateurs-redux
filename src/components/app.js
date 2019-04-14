52c04a2389c20077296cf6140e08b40b21f0f493
Cyril Marchive <cyril.marchive@gmail.com>
2019-04-14 02:08:27 -0400

    Affiche le detail d'un utilisateur

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import UserList from '../containers/user-list';
+import UserDetail from "../containers/user-detail";
 
 class App extends Component {
     render() {
         return (
             <div>
-                <UserList />
+                <UserList /><UserDetail />
             </div>
         );
     }
